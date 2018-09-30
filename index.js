fc045bfc658c8a02ac3f41f406d588ac0189e86e
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-09-30 13:41:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var ServerID = "495677413464997889"; //اي دي السيرفر
-var ChannelID = "495677521346428930";// اي دي الروم
+var ServerID = "495904655004663810"; //اي دي السيرفر
+var ChannelID = "495904655004663808";// اي دي الروم
 
 
 const Discord = require('discord.js');
